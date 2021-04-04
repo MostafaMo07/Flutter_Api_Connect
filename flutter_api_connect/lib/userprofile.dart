1deaf9ef60b9fb80b151fc8750def23af4afdeb8
mostafa <mostafa.morsy@msa.edu.eg>
2021-04-04 23:15:34 +0200

    serlize

--- flutter_api_connect/lib/userprofile.dart
+++ flutter_api_connect/lib/userprofile.dart
@@ -1,4 +1,17 @@
 import 'dart:convert';
+import 'package:json_annotation/json_annotation.dart';
+
+// class UserProfile {
+//   String firstName;
+//   String lastName;
+//   int loyaltyPoints;
+//   int fitnessGoal;
+//   bool isActive;
+
+//   UserProfile() {
+//     this.isActive = false;
+//   }
+// }
 
 class UserProfile {
   String firstName;
